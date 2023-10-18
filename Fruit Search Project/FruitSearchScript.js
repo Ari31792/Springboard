97cfc0fe003ab1f177522728ddb274d3a254a9f2
Ari Colon <ari31792@gmail.com>
2023-10-18 12:59:17 -0400

    Added Fruit Search Project

---
+++ Fruit Search Project/FruitSearchScript.js
@@ -0,0 +1,44 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	const val = str.toLowerCase();
+
+	for(i = 0; i < fruit.length; i++){
+		if(fruit[i].toLocaleLowerCase().indexOf(val) > -1){
+			results.push(fruit[i]);
+		}
+	}
+	return results;
+}
+
+function searchHandler(e) {
+	const inputVal = e.target.value;
+	let results = [];
+	if(inputVal.length > 0) {
+		results = search(inputVal);
+	}
+	showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '' ;
+	return results.forEach(v => {
+		let sugg = document.createElement('li');
+		sugg.innerHTML = v.toLowerCase().replaceAll(input.value.toLowerCase(),`<b>${input.value.toLowerCase()}</b>`);
+		
+		suggestions.append(sugg);
+	})
+}
+
+function useSuggestion(e) {
+	const sug = e.target.innerText;
+	input.value = sug;
+	suggestions.innerHTML = '';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
